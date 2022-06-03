a0c770f63b0f70c23a0b69b39e0e9d9182f8322d
Rishi Sanghvi <rishisanghvi2@gmail.com>
2022-06-03 16:27:36 +0530

    dimension8

--- src/components/onboarding/onboarding.jsx
+++ src/components/onboarding/onboarding.jsx
@@ -1,12 +1,5 @@
 import styled from "styled-components";
 import { AiFillHeart } from "react-icons/ai";
-import { FaEnvelope } from "react-icons/fa";
-
-import Work11 from "../../assets/11.webp";
-import Work12 from "../../assets/12.webp";
-import Work13 from "../../assets/13.webp";
-import Work14 from "../../assets/14.webp";
-import Work15 from "../../assets/15.png";
 
 import ProfilePic from "../../assets/2940e1d2fed0cec1277ff5bd6f00ddee.webp";
 import Work1 from "../../assets/1.webp";
@@ -148,38 +141,6 @@ const Onboarding = () => {
       <PersonalText>
         <Description>{arr}</Description>
       </PersonalText>
-      <UpperContainer>
-        <ProfilePicture>
-          <Line1 />
-          <Image4 src={ProfilePic} />
-          <Line2 />
-        </ProfilePicture>
-        <H2>Eren ✦</H2>
-        <P6>Let's create something amazing together ✦</P6>
-        <Button>
-          <FaEnvelope
-            style={{
-              position: "relative",
-              top: "1.5px",
-              right: "4px",
-            }}
-          />
-          Hire Me
-        </Button>
-      </UpperContainer>
-      <AdditionalInfo>
-        <TextDiv>
-          <P4>More by Eren ✦</P4>
-          <P5>View Profile</P5>
-        </TextDiv>
-        <ImageDiv>
-          <Image2 src={Work11} />
-          <Image2 src={Work12} />
-          <Image2 src={Work13} />
-          <Image2 src={Work14} />
-        </ImageDiv>
-        <Image3 src={Work15} />
-      </AdditionalInfo>
     </Container>
   );
 };
@@ -190,14 +151,14 @@ const Container = styled.div`
   /* margin-left: 24%;
   margin-right: 23.4%; */
   padding-top: 25px;
-  align-items: center;
-  justify-content: center;
-  align-items: center;
+  width: 80%;
+  position: relative;
+  left: 250px;
 `;
 const Profile = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 81%;
+  width: 70%;
 `;
 const Info = styled.div`
   display: flex;
@@ -287,7 +248,8 @@ const WorkProfile = styled.div`
   display: flex;
   overflow: hidden;
   position: relative;
-  width: 81%;
+
+  width: 69%;
   height: 100%;
   &:hover .abcd {
     transition: 0.5s ease;
@@ -370,124 +332,4 @@ const A = styled.a`
   &:hover {
     opacity: 0.8;
   }
-`;
-
-const UpperContainer = styled.div`
-  align-items: center;
-  justify-content: center;
-  display: grid;
-  padding-top: 60px;
-  position: relative;
-`;
-const H2 = styled.h2`
-  position: relative;
-  left: 85px;
-  font-size: 20px;
-  font-weight: 500;
-  bottom: 5px;
-`;
-const P6 = styled.p`
-  position: relative;
-  left: 12px;
-  font-size: 10px;
-  bottom: 25px;
-`;
-
-const Button = styled.button`
-  background-color: #e94c89;
-  color: white;
-  border: none;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 12px;
-  margin: 5px;
-  height: 32px;
-  width: 83px;
-  border-radius: 6px;
-  font-weight: bold;
-  cursor: pointer;
-  position: relative;
-  left: 70px;
-  bottom: 25px;
-`;
-
-const Image4 = styled.img`
-  border-radius: 50%;
-  height: 59px;
-  width: 59px;
-  position: relative;
-  bottom: 9px;
-  cursor: pointer;
-  left: 85px;
-`;
-
-const Line1 = styled.div`
-  width: 37%;
-  height: 47px;
-  border-bottom-style: solid;
-  border-bottom-width: 1.8px;
-  border-bottom-color: #e7e7e9;
-  position: absolute;
-  left: 9.5%;
-  top: 35px;
-`;
-
-const Line2 = styled.div`
-  width: 37%;
-  height: 47px;
-  border-bottom-style: solid;
-  border-bottom-width: 1.8px;
-  border-bottom-color: #e7e7e9;
-  position: absolute;
-  right: 8%;
-  top: 35px;
-`;
-
-const ProfilePicture = styled.div`
-  display: flex;
-  /* position: relative; */
-`;
-
-const AdditionalInfo = styled.div`
-  display: grid;
-  margin-right: 110px;
-  margin-left: 110px;
-  margin-top: 30px;
-`;
-const TextDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const P4 = styled.p`
-  font-size: 13px;
-  font-weight: bolder;
-`;
-const P5 = styled.p`
-  cursor: pointer;
-  font-size: 11px;
-  color: #e94c89;
-  &:hover {
-    opacity: 0.7;
-  }
-`;
-
-const ImageDiv = styled.div`
-  display: flex;
-`;
-
-const Image2 = styled.img`
-  height: 100%;
-  width: 22%;
-  margin-right: 35px;
-  border-radius: 4%;
-  cursor: pointer;
-`;
-
-const Image3 = styled.img`
-  height: 50%;
-  width: 10%;
-  margin-top: 40px;
-  position: relative;
-  left: 407px;
 `;
